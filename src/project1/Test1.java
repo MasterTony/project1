93cbb40947e1c9caf381f0b21df7ab9815f6d865
Tony Marti <ponko73@hotmail.com>
2024-01-25 13:18:15 -0600

    first commit

---
+++ src/project1/Test1.java
@@ -0,0 +1,16 @@
+package project1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello");
+		
+		Employee e1 = new Employee("Tony", 40);
+		Employee e2 = new Employee("Tony", 40);
+		
+		System.out.println(e1.equals(e2));
+	}
+
+}
